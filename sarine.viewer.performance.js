8ce11b7b4e3b16b9066f96524690cc5dd04263fb
carmeladi <carmeladi@gmail.com>
2015-02-11 17:37:25 +0200

    add sarine.viewer.performance file

---
+++ sarine.viewer.performance.js
@@ -0,0 +1,134 @@
+if (window.performance == undefined || window.performance.now == undefined) {
+    window.performance = {}
+    window.performance.now = (function() {
+        return performance.now ||
+            performance.mozNow ||
+            performance.msNow ||
+            performance.oNow ||
+            performance.webkitNow ||
+            function() {
+                return new Date().getTime();
+            };
+    })();
+}
+
+
+if (window.performance.mark == undefined) {
+    window.performance._marks = []
+
+    window.performance.mark = function(mark_name, duration) {
+        duration = duration || 0
+        window.performance._marks.push({
+            mark: mark_name,
+            startTime: window.performance.now() - document.initTime,
+            duration: duration
+        })
+    }
+
+    window.performance.getEntriesByName = function(name) {
+        return window.performance._marks.filter(function(mark) {
+            return mark.mark == name;
+        })
+    }
+
+    window.performance.clearMarks = function() {}
+    window.performance.clearMeasures = function() {}
+
+    window.performance.measure = function(new_mark, start_mark_name, end_mark_name) {
+        var start_mark = window.performance.getEntriesByName(start_mark_name)[0]
+        var end_mark = window.performance.getEntriesByName(end_mark_name)[0]
+        if(typeof start_mark !== 'undefined' && typeof end_mark !== 'undefined')
+            window.performance.mark(new_mark, end_mark.startTime - start_mark.startTime)
+        else
+            window.performance.mark(new_mark,new Date().getTime() - new Date().getTime())
+    }
+}
+
+document.initTime = performance.now();
+window.performance.mark("mark_start")
+
+
+
+var performanceManager = (function(){
+    
+    function formatTime(totalTime){
+        return (totalTime / 1000).toFixed(3) + "s";
+    }
+
+    function writeToLog(id, totalTime){        
+        $('#' + id + '>.value').html(performanceManager.FormatTime(totalTime))
+    }
+    function calcAndWriteToLog(id){
+    $('#' + id + '>.value').html(performanceManager.FormatTime(calcTime(id)))
+    }
+
+    function isDebugMode(){
+        return location.hash.indexOf("debug") == 1;
+    }
+
+    function measure(id){    
+        window.performance.measure(id);    
+    }
+
+    function mark(eventName){
+        window.performance.measure(eventName);
+    }
+
+    function calcTime(eventName){
+        var measure = window.performance.getEntriesByName(eventName)[0];
+        return measure.duration + measure.startTime;
+    }
+
+    function init(viewersArr){           
+         var ul = document.createElement('ul');
+         ul.id = 'debug_log';
+         ul.style.position = "absolute";
+         ul.style.bottom ="0"
+         ul.style.background = "#ccc"
+         
+         for (var i=0; i < viewersArr.length; i++){
+
+            //first init
+            var li= document.createElement('li');
+            var span = document.createElement('span');
+            span.innerText = 'loading...';
+            span.className = 'value';
+            li.id = viewersArr[i].id + '_' + viewersArr[i].element.data('type') + '_first_init';
+            li.innerHTML = viewersArr[i].id + '_' + viewersArr[i].element.data('type') +  '_first_init : ' + span.outerHTML;
+            ul.appendChild(li);       
+
+            //full init
+            var li2= document.createElement('li');
+            var span = document.createElement('span');
+            span.innerText = 'loading...';
+            span.className = 'value';
+            li2.id = viewersArr[i].id + '_' + viewersArr[i].element.data('type') + '_full_init';
+            li2.innerHTML = viewersArr[i].id + '_' + viewersArr[i].element.data('type') +  '_full_init : ' + span.outerHTML;
+            ul.appendChild(li2);
+         }
+         document.body.appendChild(ul);
+         $("#debug_log").show();
+    }
+
+    return {
+        FormatTime : formatTime,
+        IsDebugMode : isDebugMode,
+        WriteToLog : writeToLog,
+        Measure : measure,
+        Mark : mark,
+        CalcAndWriteToLog : calcAndWriteToLog,        
+        Init : init
+    }
+})();
+
+
+$(function() { 
+    $(document).on("loadDebugger",function(){        
+        if(performanceManager.IsDebugMode())
+            performanceManager.Init(vm.getViewers());      
+    })
+});
+
+
+   
+
