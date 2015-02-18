9697c2dfb9e22bf479570d2fa1e58198c9abd6b9
carmeladi <carmeladi@gmail.com>
2015-02-18 11:09:28 +0200

    Version 0.0.6

---
+++ Gruntfile.js
@@ -0,0 +1,103 @@
+'use strict';
+module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt)
+    var files = ["Gruntfile.js", "package.json", "dist/*.js", "src/*", "bower.json", "release.cmd", "commit.cmd"]
+    var message = "commit"
+    grunt.initConfig({
+        config: grunt.file.readJSON("bower.json"),
+        version: {
+            project: {
+                src: ['bower.json', 'package.json']
+            }
+        },
+        copy: {
+            build: {
+                flatten: true,
+                src: ["src/*.png"],
+                dest: "dist",
+                expand: true
+            }
+        },
+        concat: {
+            build: {
+                src: ["src/*.js"],
+                dest: 'dist/sarine.viewer.utils.js'
+            }     
+        },      
+        uglify: {
+            build: {
+                options: {
+                    mangle: false
+                },
+                files: {
+                    'dist/sarine.viewer.utils.min.js': ['dist/sarine.viewer.utils.js']
+                }
+            }
+        },
+        gitcommit: {
+            all: {
+                options: {
+                    message: "<%= config.message %>",
+                    force: true
+                },
+                files: {
+                    src: files
+                }
+            },
+            bower: {
+                options: {
+                    message: "release : <%= config.version %>",
+                    force: true
+                },
+                files: {
+                    src: ["bower.json", "package.json"]
+                }
+            }
+        },
+        gitpush: {
+            all: {
+                options: {
+                    force: true
+                },
+                files: {
+                    src: files
+                }
+            }
+        },
+        gitadd: {
+            firstTimer: {
+                option: {
+                    force: true
+                },
+                files: {
+                    src: files
+                }
+            }
+        },
+        gitpull: {
+            build: {
+                options: {
+                    force: true
+                },
+                files: {
+                    src: files
+                }
+            }
+        },
+        prompt: {
+            all: {
+                options: {
+                    questions: [{
+                        config: 'config.message',
+                        type: 'input',
+                        message: 'comment:\n',
+                        default: 'commit'
+                    }]
+                }
+            }
+        }
+    })
+    grunt.registerTask('build', ['copy','concat','uglify']);
+    grunt.registerTask('commit', ['prompt', 'gitadd', 'gitcommit:all', 'gitpush']);
+    grunt.registerTask('release-git', ['version:project:patch', 'gitcommit:bower', 'release']);
+};
