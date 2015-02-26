0ca8359a54742f82648fb502270f81a3aa4cc0ac
carmeladi <carmeladi@gmail.com>
2015-02-26 13:39:23 +0200

    block ie9 only

--- src/jquery.reject.init.js
+++ src/jquery.reject.init.js
@@ -1,7 +1,20 @@
 $(function() {
      if (typeof utilsManager !== 'undefined'){
     	if(!utilsManager.IsMobile()){ 
-    		 
+    		  $.reject({
+                reject: {
+                    msie:9                   
+                },
+                close: false, // Prevent closing of window ,
+                header: 'Your browser version is out of date',
+                display: ['chrome', 'safari', 'firefox', 'msie'],
+                imagePath:'//d3oayecwxm3wp6.cloudfront.net/qa2/content/viewers/shell/v1/images/'
+            });
+
+    		 if(typeof vm !== 'undefined') //stop viewers
+    		 	vm.stop();
+
+            return false;
     	}
     }
 });
